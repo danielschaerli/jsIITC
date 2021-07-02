dcdf2ecd70375bc5ab769f8b9b3891396f32e1cb
danielschaerli <daniel.schaerli@hispeed.ch>
2021-07-02 17:31:05 +0200

    Default plugin

--- script.js
+++ script.js
@@ -5,39 +5,43 @@
 // @version 0.0.1
 // @namespace https://tempuri.org/iitc/hello
 // @description Hello, World plugin for IITC
-// @include https://intel.ingress.com/intel *
-// @match https://intel.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
 // @grant none
+
 // ==/UserScript==
 // Wrapper function that will be stringified and injected
 // into the document. Because of this, normal closure rules
 // do not apply here.
 function wrapper(plugin_info) {
-// Make sure that window.plugin exists. IITC defines it as a no-op function,
-// and other plugins assume the same.
     if(typeof window.plugin !== 'function') window.plugin = function() {};
-// Name of the IITC build for first-party plugins
+
+    // PLUGIN START ////////////////////////////////
+
     plugin_info.buildName = 'hello';
-// Datetime-derived version of the plugin
     plugin_info.dateTimeVersion = '20150829103500';
-// ID/name of the plugin
     plugin_info.pluginId = 'hello';
-// The entry point for this plugin.
+
+    // The entry point for this plugin.
     function setup() {
         alert('Hello, IITC!');
     }
-// Add an info property for IITC's plugin system
-    setup.info = plugin_info;
-// Make sure window.bootPlugins exists and is an array
-    if (!window.bootPlugins) window.bootPlugins = [];
-// Add our startup hook
+
+    setup.info = plugin_info; // Add an info property for IITC's plugin system
+    if (!window.bootPlugins) window.bootPlugins = []; // Make sure window.bootPlugins exists and is an array
+    // Add our startup hook
     window.bootPlugins.push(setup);
-// If IITC has already booted, immediately run the 'setup' function
+    // If IITC has already booted, immediately run the 'setup' function
     if (window.iitcLoaded && typeof setup === 'function') setup();
 }
+
+// PLUGIN END //////////////////////////////////////////////////
+
 // Create a script element to hold our content script
-var script = document.createElement('script');
-var info = {};
+let script = document.createElement('script');
+let info = {};
 // GM_info is defined by the assorted monkey-themed browser extensions
 // and holds information parsed from the script header.
 if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
@@ -47,6 +51,7 @@ if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
         description: GM_info.script.description
     };
 }
+
 // Create a text node and our IIFE inside of it
 let textContent = document.createTextNode('('+ wrapper +')('+ JSON.stringify(info) +')˓→');
 // Add some content to the script element
